8601b6fb0dbd2713b1214531b68f50f1909f7c7a
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-07-10 09:19:48 -0300

    Evento no todo-form, pegando valor e armazenando em variável

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,12 @@ const cancelEditBtn = document.querySelector("#cancel-edit-btn")
 // Funções
 
 // Eventos
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputValue = todoInput.value;
+
+    if(inputValue) {
+        console.log(inputValue)
+    }
+})
